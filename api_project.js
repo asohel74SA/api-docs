9c48a03fa259a8c7454194e419f675cd842f52ef
alexandr <alexnaum@gmail.com>
2017-12-15 17:12:17 +0300

    updates

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-11-08T15:29:36.084Z",
+    "time": "2017-12-13T15:24:58.666Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
